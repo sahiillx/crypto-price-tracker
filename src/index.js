7f0f19dbce81c5601c84f774e46763b5509d6fc0
Sahil Khan <sahilkhan0471@gmail.com>
2024-04-19 14:58:02 +0530

    root position fixed

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import { ChakraProvider, theme } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>
 );
 
 export const server = `https://api.coingecko.com/api/v3`;
