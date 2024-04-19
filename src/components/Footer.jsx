f3cdd999a869058785d4d5adb274be50a79ebf71
Sahil Khan <sahilkhan0471@gmail.com>
2024-04-19 14:46:29 +0530

    ready to deploy

---
+++ src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+import founder1 from "../assets/founder1.jpg";
+import founder2 from "../assets/pf.png";
+import founder3 from "../assets/founder3.jpg";
+import founder4 from "../assets/founder4.jpg";
+
+
+
+
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            We are the best crypto trading app in India, we provide our guidance
+            at a very cheap price.
+          </Text>
+        </VStack>
+
+        <VStack flexDirection="row">
+          <Avatar boxSize={"28"} mt={["4", "0"]} style={{ marginRight: "20px" }} src={founder1} />
+          <Avatar boxSize={"28"} mt={["4", "0"]} style={{ marginRight: "20px" }} src={founder3} />
+          <Avatar boxSize={"28"} mt={["4", "0"]} style={{ marginRight: "20px" }} src={founder4} />
+          <Avatar boxSize={"28"} mt={["4", "0"]} style={{ marginRight: "20px" }} src={founder2} />
+          <Text>Our Founders</Text>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
