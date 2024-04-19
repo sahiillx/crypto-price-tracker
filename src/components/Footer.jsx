ed55c18f36c97156689262bebe2de35327af8805
Sahil Khan <sahilkhan0471@gmail.com>
2024-04-19 15:34:03 +0530

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,6 @@ import founder2 from "../assets/pf.png";
 import founder3 from "../assets/founder3.jpg";
 import founder4 from "../assets/founder4.jpg";
 
-
-
-
-
 const Footer = () => {
   return (
     <Box
@@ -30,12 +26,12 @@ const Footer = () => {
             at a very cheap price.
           </Text>
         </VStack>
-
-        <VStack flexDirection="row">
-          <Avatar boxSize={"28"} mt={["4", "0"]} style={{ marginRight: "20px" }} src={founder1} />
-          <Avatar boxSize={"28"} mt={["4", "0"]} style={{ marginRight: "20px" }} src={founder3} />
-          <Avatar boxSize={"28"} mt={["4", "0"]} style={{ marginRight: "20px" }} src={founder4} />
-          <Avatar boxSize={"28"} mt={["4", "0"]} style={{ marginRight: "20px" }} src={founder2} />
+       
+        <VStack spacing="4">
+          <Avatar boxSize={["24", "28"]} mt={["4", "0"]} src={founder2} />
+          <Avatar boxSize={["24", "28"]} mt={["4", "0"]} src={founder1} />
+          <Avatar boxSize={["24", "28"]} mt={["4", "0"]} src={founder3} />
+          <Avatar boxSize={["24", "28"]} mt={["4", "0"]} src={founder4} />
           <Text>Our Founders</Text>
         </VStack>
       </Stack>
